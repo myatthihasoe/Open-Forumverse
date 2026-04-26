6828aa7fe60b7d81b126d75dcfc4eb3ede97b05f
Xuan <myatthiha.soe.9@gmail.com>
2026-04-26 13:01:13 +0630

    connect MongoDB and create UserSchema

---
+++ database/user.model.ts
@@ -0,0 +1,52 @@
+import { model, Schema } from "mongoose";
+
+interface UserType {
+  name: string;
+  username: string;
+  email: string;
+  bio?: string;
+  image: string;
+  location?: string;
+  portfolio?: string;
+  reputation?: number;
+}
+
+const UserSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    bio: {
+      type: String,
+    },
+
+    image: {
+      type: String,
+      required: true,
+    },
+    location: {
+      type: String,
+    },
+    portfolio: {
+      type: String,
+    },
+    reputation: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = model<UserType>("User", UserSchema);
+export default User;
