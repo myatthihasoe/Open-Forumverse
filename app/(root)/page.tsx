afe47f9e372cd130f7e1b9384f5b90ab532aac53
Xuan <myatthiha.soe.9@gmail.com>
2026-05-31 13:21:11 +0630

    reusable filter component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,11 @@
 // 'use client'
 import { auth } from "@/auth";
 import ButtonLink from "@/components/ButtonLink";
+import CommonFilter from "@/components/CommonFilter";
 import DataRenderer from "@/components/DataRenderer";
 import Filters from "@/components/Filters";
 import ThreadCard from "@/components/ThreadCard";
+import { HomePageFilters, DefaultFilters } from "@/constant/filters";
 import { GetDiscussion } from "@/lib/actions/GetDiscussion.action";
 import { api } from "@/lib/api";
 import ROUTES from "@/routes";
@@ -16,15 +18,14 @@ export default async function page({
   const session = await auth();
   const { page, pageSize, filter, search } = await searchParams;
 
-  const {success, data, message, details} = await GetDiscussion({
+  const { success, data, message, details } = await GetDiscussion({
     page: Number(page) || 1,
     pageSize: Number(pageSize) || 10,
     filter: filter || "",
     search: search || "",
   });
 
-  const {questions = []} = data || {}
-
+  const { questions = [] } = data || {};
 
   return (
     <>
@@ -33,19 +34,28 @@ export default async function page({
           <h1 className="font-extrabold text-3xl text-white">All Threads</h1>
         </div>
         <div>
+          <CommonFilter
+            filters={HomePageFilters}
+            defaultFilter={DefaultFilters.HomePageFilters}
+          />
+        </div>
+        <div>
           <ButtonLink href={ROUTES.DISCUSSION_CREATE}>
             Create New Thread
           </ButtonLink>
         </div>
       </div>
       <Filters />
-      <DataRenderer 
+      <DataRenderer
         success={success}
         data={questions}
         errorMessage={message}
-          render={(questions) => questions?.map((question,i) => (
+        render={(questions) =>
+          questions?.map((question, i) => (
             <ThreadCard key={i} question={question} />
-          ))} />
+          ))
+        }
+      />
     </>
   );
 }
